1649db8087501e1f80015e05705993eaa4795d56
Nicholas Crandall <ncrandall@gmail.com>
2013-05-01 14:11:17 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Todont App
+==========
+
+
+This project is mainly for learning how to create a rails RESTful API. This API will be used to create a mobile app using appcelerator. This project is meant as a reference and should is not recommended for production use OOTB.
+
+Rails Version: 4.0.0.beta1
+
+Ruby Version: 2.0.0.p1
+
+Database: 
+
+* Development: SQLite
+
+* Production: PostgreSQL
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todont::Application.config.secret_key_base = '1dfd25b63943c8eeeb5d82ff6fa535666bf24087cc12afcc4418775f0e4b42642ec770fb0ba869d0d23f3aa71d3ab6c4de934cc71d896e4f876251984463a3db'
